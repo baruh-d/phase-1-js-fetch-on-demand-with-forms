5690f4fccb50e3603f1c708cc8e5c09e89fcafb6
baru-d <barudaniel6@gmail.com>
2023-10-13 14:18:33 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 const init = () => {
-  
-}
-
+    const inputDOM = document.querySelector('form');
+    inputDOM.addEventListener('submit', (e) => e.preventDefault());
+    const searchDOM = document.querySelector('#searchByID');
+    const title = document.querySelector('section#movieDetails h4');
+    const summary = document.querySelector('section#movieDetails p');
+    fetch(`http://localhost:3000/movies/${searchDOM.value}`)
+      .then((res) => res.json)
+      .then((response) => {
+        title.innerText = response.title;
+        summary.innerText = response.summary;
+      });
+};
 document.addEventListener('DOMContentLoaded', init);
